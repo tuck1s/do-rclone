a72c93d3e39fda461ec40d36794e4c6202483f2f
tuck1s <steve@thetucks.com>
2025-12-13 15:17:17 +0000

    updated to use copy for fewer dir lists

--- do-rclone.sh
+++ do-rclone.sh
@@ -5,7 +5,7 @@ logfile=$logdir/rclone.log
 /usr/sbin/logrotate $logdir/.logrotaterc --state $logdir/.logrotate.status -f
 
 #
-for d in Kathy Hannah Olly Steve Qmultimedia Music
+for d in Kathy Hannah Olly Steve Qmultimedia
 do
   echo ====== Starting \"$d\" >>$logfile
   # need to specify a modify time window of 2 seconds to prevent needless "modification time" updates
@@ -14,10 +14,17 @@ do
   #   now we have more virtual memory set up, use more checkers and transfers (was: 2, 2)
   #   use terse one-line stats logging
 
-  rclone sync /mnt/$d/ backblaze:/TuckStore-$d --exclude-from "$logdir/rclone_exclude" --verbose --checkers=8 --transfers=4 --modify-window=2s \
-    --buffer-size 64M --config="$logdir/rclone.conf" --log-file=$logfile --stats-one-line
+  rclone copy /mnt/$d/ backblaze:/TuckStore-$d \
+    --fast-list \
+    --ignore-existing \
+    --exclude-from "$logdir/rclone_exclude" \
+    --cache-dir "$logdir/rclone-cache-$d" \
+    --cache-tmp-upload-path "$logdir/rclone-cache-tmp-$d" \
+    --config="$logdir/rclone.conf" \
+    --verbose --checkers=8 --transfers=4 --modify-window=2s \
+    --buffer-size 64M --log-file=$logfile --stats-one-line --dry-run
 done
 echo ====== Done >>$logfile
 swaks --server smtp.ntlworld.com --to steve@thetucks.com --ehlo there --from steven.tuck@ntlworld.com --header "Subject: rclone backup complete" \
-  --body "Log file attached." --attach-name $logfile.txt --attach @$logfile --tlsc --silent 1
+  --body "Log file attached." --attach-name $logfile.txt --attach @$logfile --tlsc --auth-user steven.tuck@ntlworld.com --auth-pass i2pkEI3H4v --silent 1
 
