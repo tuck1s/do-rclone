cb350afaed904911e98a55751f890da2091efb46
tuck1s <steve@thetucks.com>
2025-12-13 16:19:05 +0000

    more work on cleanup

--- cleanup-recycle.sh
+++ cleanup-recycle.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
 logdir=$HOME/do-rclone
 logfile=$logdir/rclone.log
+# Control whether this actually deletes, or just does a dry run
+DRY_RUN_FLAG="--dry-run"
+# DRY_RUN_FLAG=""
+
 # force the log to rotate before starting
 /usr/sbin/logrotate $logdir/.logrotaterc --state $logdir/.logrotate.status -f
 
 #
+# Cleanup script REMOVES all files from the Backblaze buckets, that match our exclude list
+#
 for d in Kathy Hannah Olly Steve Qmultimedia
 do
-  echo ====== CLEANUP of recycle areas \"$d\" >>$logfile
-  # need to specify a modify time window of 2 seconds to prevent needless "modification time" updates
+  echo ====== CLEANUP $DRY_RUN_FLAG of junk files and recycle areas \"$d\" >>$logfile
   #   use separate buckets for each share, for quicker browsing on BackBlaze UI
   #   set the config file directly, for compatibility with crontab
-  #   now we have more virtual memory set up, use more checkers and transfers (was: 2, 2)
+  #   now we have more virtual memory set up, use more checkers and transfers (was: 2, 2)./x
   #   use terse one-line stats logging
-  #   try to optimise costs with --fast-list, ignore-existing and cache
   rclone delete backblaze:/TuckStore-$d \
-    --include "#recycle/**" \
+    --include-from "$logdir/cleanup_list" \
     --config="$logdir/rclone.conf" \
     --verbose --checkers=8 --transfers=4 --modify-window=2s \
-    --buffer-size 64M --log-file=$logfile --stats-one-line
+    --buffer-size 64M --log-file=$logfile --stats-one-line $DRY_RUN_FLAG
 done
 echo ====== Done >>$logfile
 swaks --server smtp.ntlworld.com --to steve@thetucks.com --ehlo there --from steven.tuck@ntlworld.com --header "Subject: rclone backup complete" \
